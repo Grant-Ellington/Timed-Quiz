8372c48d6e39fe76a2943acb618c2d2ec46cf0a0
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-10 15:51:03 -0600

    added start buton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log('hello World')
+var questionArr = [
+     question1 = {
+        question:'This is the first question',
+        options: [ 'a','b','c','d'],
+        answer: 'a'
+    },
+     question2 = {
+        question:'This is the second question',
+        options: [ 'e','f','g','h'],
+        answer: 'e'
+    }
+];
+console.log(questionArr[0].options)
+
+timeLeft = 100;
+
+var startButton = document.querySelector('#start-button')
+
+function startTimer(){
+    console.log('started')
+}
+
+startButton.addEventListener('click', startTimer)
